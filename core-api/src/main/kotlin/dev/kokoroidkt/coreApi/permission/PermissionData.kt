1e4ec0344b8d0656f5d1710654af4aa13a1ff053
moran0710 <3575331055@qq.com>
2026-03-13 23:27:49 +0800

    revert(premission): 删除现有premission实现，准备重写实现

--- core-api/src/main/kotlin/dev/kokoroidkt/coreApi/permission/PermissionData.kt
+++
@@ -1,8 +0,0 @@
-package dev.kokoroidkt.coreApi.permission
-
-data class PermissionData(
-    val permissionString: String,
-    val permission: PermissionValue,
-) {
-    override fun toString(): String = "$permissionString: $permission"
-}
