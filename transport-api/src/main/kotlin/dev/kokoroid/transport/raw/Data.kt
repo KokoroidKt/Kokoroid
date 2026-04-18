491cc3799c468af843bf8c0b135fa762a612ffc7
moran0710 <3575331055@qq.com>
2026-04-18 19:32:38 +0800

    chore(docs): 为Raw和Data类新增注释

--- transport-api/src/main/kotlin/dev/kokoroid/transport/raw/Data.kt
+++ transport-api/src/main/kotlin/dev/kokoroid/transport/raw/Data.kt
@@ -8,7 +8,18 @@ package dev.kokoroid.transport.raw
 
 import kotlinx.serialization.json.JsonElement
 
+/**
+ * 承载的数据
+ *
+ * @constructor Create empty Data
+ */
 sealed class Data {
+    /**
+     * Json数据
+     *
+     * @property json
+     * @constructor Create empty Json
+     */
     data class Json(
         val json: JsonElement,
     ) : Data()
--- transport-api/src/main/kotlin/dev/kokoroid/transport/raw/Raw.kt
+++ transport-api/src/main/kotlin/dev/kokoroid/transport/raw/Raw.kt
@@ -6,6 +6,13 @@
 
 package dev.kokoroid.transport.raw
 
+/**
+ * 驱动器收到的原始请求
+ *
+ * @property data 原始数据
+ * @property attribute 附加属性，用于标识一些传输过程的信息，可以通过[AttrKey]查看一些内置的约定
+ * @constructor Create empty Raw
+ */
 data class Raw(
     val data: Data,
     val attribute: Map<String, String>,
