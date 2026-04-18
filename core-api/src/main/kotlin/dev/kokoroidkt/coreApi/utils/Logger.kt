eb7b6776c4fabed5758da83abe85ffcd708aef95
moran0710 <3575331055@qq.com>
2026-04-18 23:28:24 +0800

    chore(core): 优化Logger获取方法，支持默认名称

--- core-api/src/main/kotlin/dev/kokoroidkt/coreApi/utils/Logger.kt
+++ core-api/src/main/kotlin/dev/kokoroidkt/coreApi/utils/Logger.kt
@@ -2,18 +2,21 @@ package dev.kokoroidkt.coreApi.utils
 
 import dev.kokoroidkt.coreApi.classloader.ExtensionClassloader
 import dev.kokoroidkt.coreApi.logging.KokoroidLogger
+import dev.kokoroidkt.coreApi.logging.LoggerFactory
+import org.koin.java.KoinJavaComponent.getKoin
 
 /**
  * 拓展获得Logger的简易方法
  * Logger被注入了ExtensionClassLoader，此处可以直接取出
  * 此拓展函数只能由Kokoroid加载的拓展使用，否则会抛出IllegalStateException
  *
+ * @param default 默认Logger名称，当无法获取到Logger时使用
  * @return 对应Logger
  */
-fun Any.getExtensionLogger(): KokoroidLogger {
+fun Any.getExtensionLogger(default: String = "Extension"): KokoroidLogger {
     val classLoader =
         this::class.java.classLoader
-            ?: throw IllegalStateException("Class loader cannot be null for object: ${this::class.java.simpleName}")
-    if (classLoader !is ExtensionClassloader) throw IllegalStateException("Class loader must be ExtensionClassloader")
+            ?: return getKoin().get<LoggerFactory>().invoke(default, this::class)
+    if (classLoader !is ExtensionClassloader) return getKoin().get<LoggerFactory>().invoke(default, this::class)
     return classLoader.logger
 }
